ea706fead22ccf25f840aa3d3b1b858dabc38e35
me <a@a.a>
2022-07-18 13:35:21 -0700

    working looping scroll

--- script.js
+++ script.js
@@ -10,21 +10,16 @@ window.addEventListener('scroll', () => {
 
 
 
-const mySkills = ["HTML", "CSS", "JavaScript", "Bootstrap", "Git", "Python", "Linux", "Node", "mySQL", "Spring", "HTML", "CSS", "JavaScript", "Bootstrap", "Git", "Python", "Linux", "Node", "mySQL", "Spring"];
-animUl = document.getElementById("anim-ul");
-animUl2 = document.getElementById("anim-ul-2");
-animUl3 = document.getElementById("anim-ul-3");
-animUl4 = document.getElementById("anim-ul-4");
+const mySkills = ["HTML", "CSS", "JavaScript", "Bootstrap", "Git", "Python", "Linux", "Node", "mySQL", "Spring"];
+const animUl = document.getElementById("anim-ul");
+const animUl2 = document.getElementById("anim-ul-2");
 
-// generate enough that it wraps without snapping
-for (let i = 0; i < 3; i++) {
-  console.log(i);
+// generate enough content that it wraps without snapping
+for (let i = 0; i < 4; i++) {
+  mySkills.forEach(item => {
+    let listItem = document.createElement("li");
+    listItem.textContent = item;
+    animUl.appendChild(listItem);
+    animUl2.appendChild(listItem.cloneNode(true));
+  });
 }
-mySkills.forEach(item => {
-  let listItem = document.createElement("li");
-  listItem.textContent = item;
-  animUl.appendChild(listItem);
-  animUl2.appendChild(listItem.cloneNode(true));
-  animUl3.appendChild(listItem.cloneNode(true));
-  animUl4.appendChild(listItem.cloneNode(true));
-});
