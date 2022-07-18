c9cfae5780c040157b69005e6299cec9b480db63
me <a@a.a>
2022-07-18 12:12:37 -0700

    testing scrolling again

--- script.js
+++ script.js
@@ -10,18 +10,21 @@ window.addEventListener('scroll', () => {
 
 
 
-const mySkills = ["HTML", "CSS", "JavaScript", "Bootstrap", "Git"];
+const mySkills = ["HTML", "CSS", "JavaScript", "Bootstrap", "Git", "Python", "Linux", "Node", "mySQL", "Spring", "HTML", "CSS", "JavaScript", "Bootstrap", "Git", "Python", "Linux", "Node", "mySQL", "Spring"];
 animUl = document.getElementById("anim-ul");
 animUl2 = document.getElementById("anim-ul-2");
+animUl3 = document.getElementById("anim-ul-3");
+animUl4 = document.getElementById("anim-ul-4");
 
+// generate enough that it wraps without snapping
+for (let i = 0; i < 3; i++) {
+  console.log(i);
+}
 mySkills.forEach(item => {
   let listItem = document.createElement("li");
   listItem.textContent = item;
   animUl.appendChild(listItem);
-});
-
-mySkills.forEach(item => {
-  let listItem = document.createElement("li");
-  listItem.textContent = item;
-  animUl2.appendChild(listItem);
+  animUl2.appendChild(listItem.cloneNode(true));
+  animUl3.appendChild(listItem.cloneNode(true));
+  animUl4.appendChild(listItem.cloneNode(true));
 });
